42530096c7408f8ec9de13461ffd574c0571e372
knut <knut@lickert.net>
2016-04-17 23:57:17 +0200

    Define custom easter monday and test it.
    This test fails. Probably it is a bug.

--- test/test_custom_holidays.rb
+++ test/test_custom_holidays.rb
@@ -12,6 +12,16 @@ class CustomHolidaysTest < Test::Unit::TestCase
     assert_not_equal [], Holidays.on(Date.civil(2013,6,20), :custom_single_file)
   end
 
+  def test_custom_region_present_after_easter_file_parsing
+    assert_raises Holidays::UnknownRegionError do
+      Holidays.on(Date.civil(2013,6,20), :custom_easter_file)
+    end
+    Holidays.load_custom('test/data/test_single_custom_easter_defs.yaml')
+
+    assert_equal [], Holidays.on(Date.civil(2016,3,26), :custom_easter_file)
+    assert_equal ['Easter monday'], Holidays.on(Date.civil(2016,3,27), :custom_easter_file)
+  end
+
   def test_custom_holidays_present_after_multiple_file_parsing
     assert_raises Holidays::UnknownRegionError do
       Holidays.on(Date.civil(2013, 10,5), :custom_multiple_files)
